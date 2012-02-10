f87c84b4536dd6ccffd3c2c3d30edc42f4ed2330
katebus <ktb@o2.pl>
2012-02-10 23:17:39 +0100

    gies digest, added nbproject

--- PhotoManager/src/com/sharpcrow/photomanager/FileVerifier.java
+++ PhotoManager/src/com/sharpcrow/photomanager/FileVerifier.java
@@ -1,7 +1,5 @@
 package com.sharpcrow.photomanager;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -12,48 +10,45 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class FileVerifier {
 
-	public void verify(String path) throws Exception {
-		CountingFileVisitor<Path> fv = buildFileTree(path);
+    public void verify(String path) throws Exception {
+        CountingFileVisitor<Path> fv = buildFileTree(path);
 
-		for (Path p : fv.getFiles()) {
-			System.out.println(p);
-		}
+        for (Path p : fv.getFiles()) {
+            System.out.println(p);
+        }
 
-		System.out.println(fv.getFiles().size() + ", " + fv.getSize());
+        System.out.println(fv.getFiles().size() + ", " + fv.getSize());
 
-		for (Path p : fv.getFiles()) {
-			String cksum = digest(p);
+        for (Path p : fv.getFiles()) {
+            String cksum = digest(p);
 
-			System.out.println(p + " " + cksum);
-		}
-	}
+            System.out.println(p + " " + cksum);
+        }
+    }
 
-	private String digest(Path p) throws IOException {
-		String cksum = null;
+    private String digest(Path p) throws IOException {
+        String cksum = null;
+        try (InputStream newInputStream = Files.newInputStream(p)) {
+            cksum = DigestUtils.md5Hex(newInputStream);
+        } catch (IOException ex) {
+            System.err.println("Missing file " + p);
+            throw ex;
+        }
 
-		try (InputStream newInputStream =  new FileInputStream(p.toFile()) ) {
-			cksum = DigestUtils.md5Hex(newInputStream);
-		} catch (FileNotFoundException ex) {
-			System.err.println("Missing file " + p);
+        return cksum;
+    }
 
-		}
+    private CountingFileVisitor<Path> buildFileTree(String path)
+            throws IOException {
+        Path startingDir = Paths.get(path);
+        CountingFileVisitor<Path> fv = new CountingFileVisitor<>();
+        Files.walkFileTree(startingDir, fv);
 
-		return cksum;
-	}
-
-	private CountingFileVisitor<Path> buildFileTree(String path)
-			throws IOException {
-		Path startingDir = Paths.get(path);
-		CountingFileVisitor<Path> fv = new CountingFileVisitor<Path>();
-		Files.walkFileTree(startingDir, fv);
-
-		return fv;
-	}
-
-	//	verifyImage(){
-	//		BufferedImage buffImg = ImageIO.read(p.toFile());
-	//		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-	//		ImageIO.write(buffImg, "png", outputStream);
-	//	}
-
+        return fv;
+    }
+    //	verifyImage(){
+    //		BufferedImage buffImg = ImageIO.read(p.toFile());
+    //		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    //		ImageIO.write(buffImg, "png", outputStream);
+    //	}
 }
